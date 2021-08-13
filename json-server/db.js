1f3d39156a1274d9dc805405eaee3e6469e91eab
Anouar Mahmoud BELLAKHA <usereact@gmail.com>
2021-08-13 01:31:57 +0100

    Create a mock api

---
+++ json-server/db.js
@@ -0,0 +1,48 @@
+const faker = require("faker");
+
+module.exports = () => {
+    let flights = [];
+    let contingents = [];
+
+    for (let index = 0; index < 100; index++) {
+        let currentFlightContingents = [];
+        const airplaneMaxCapacity = 800;
+        let airplaneRemainingCapacity = airplaneMaxCapacity;
+        while (airplaneRemainingCapacity) {
+            const bookedSeatsCount = faker.datatype.number(airplaneRemainingCapacity);
+            airplaneRemainingCapacity -= bookedSeatsCount;
+
+            currentFlightContingents = [
+                ...currentFlightContingents,
+                {
+                    id: faker.datatype.uuid(),
+                    clientCode: faker.datatype.uuid(),
+                    bookedSeatsCount,
+                    totalSeatsCount: faker.datatype.number({ min: bookedSeatsCount }),
+                },
+            ];
+        }
+
+        flights = [
+            ...flights,
+            {
+                id: faker.datatype.uuid(),
+                iataCarrierCode: faker.random.alpha({ count: 2 }),
+                number: faker.datatype.number(9999),
+                date: faker.date.future(),
+                origin: faker.random.alpha({ count: 3 }),
+                destination: faker.random.alpha({ count: 3 }),
+                contingents: currentFlightContingents.map((contingent) => contingent.id),
+            },
+        ];
+        contingents = [
+            ...contingents,
+            ...currentFlightContingents,
+        ];
+    }
+
+    return {
+        flights,
+        contingents,
+    };
+};
