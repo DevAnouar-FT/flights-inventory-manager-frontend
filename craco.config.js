edf6213bfb8a46df4331fb94a2da2ee2b5b501e3
Anouar Mahmoud BELLAKHA <usereact@gmail.com>
2021-08-13 05:16:34 +0100

    Add and setup tailwind

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      plugin: require("craco-alias"),
+      options: {
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.extend.json",
+      },
+    },
+  ],
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+};
